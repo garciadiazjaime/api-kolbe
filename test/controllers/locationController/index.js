c3d2a29e47f5b98cffbd189ae2b27fbb821bb294
jaime <garciadiazjaime@gmail.com>
2017-01-09 21:13:28 -0600

    lint unittest

--- test/controllers/locationController/index.js
+++ test/controllers/locationController/index.js
@@ -1,3 +1,4 @@
+/* eslint max-len: [2, 500, 4] */
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import MongoUtil from 'util-mongodb';
@@ -14,44 +15,31 @@ describe('LocationController', () => {
 
   describe("#getLocations valid case", () => {
     const validResponse = [{}];
+    const promise = new Promise((resolve) => resolve(validResponse));
 
     beforeEach(() => {
-      sinon.stub(MongoUtil.prototype, 'find', () => {
-        return new Promise((resolve) => {
-          resolve(validResponse);
-        });
-      });
+      sinon.stub(MongoUtil.prototype, 'find', () => promise);
     });
 
     afterEach(() => {
       MongoUtil.prototype.find.restore();
     });
 
-    it('valid case', () => {
-      return expect(locationController.getLocations()).to.eventually.equal(validResponse);
-    });
-
+    it('valid case', () => expect(locationController.getLocations()).to.eventually.equal(validResponse));
   });
 
   describe("#getLocations invalid case", () => {
     const invalidResponse = 'error';
+    const promise = new Promise((_, reject) => reject(invalidResponse));
 
     beforeEach(() => {
-      sinon.stub(MongoUtil.prototype, 'find', () => {
-        return new Promise((resolve, reject) => {
-          reject(invalidResponse);
-        });
-      });
+      sinon.stub(MongoUtil.prototype, 'find', () => promise);
     });
 
     afterEach(() => {
       MongoUtil.prototype.find.restore();
     });
 
-    it('invalid case', () => {
-      return expect(locationController.getLocations()).to.be.rejectedWith(invalidResponse);
-    });
-
+    it('invalid case', () => expect(locationController.getLocations()).to.be.rejectedWith(invalidResponse));
   });
-
 });
