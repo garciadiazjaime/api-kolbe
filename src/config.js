0e01a731b9a1af363ac75a5135775dcdcabe60c2
jaime <garciadiazjaime@gmail.com>
2017-05-28 18:20:25 -0500

    upload document

--- src/config.js
+++ src/config.js
@@ -74,7 +74,7 @@ const config = convict({
     default: '',
     env: 'MINT_TOKEN',
   },
-  dataFoldar: {
+  dataFolder: {
     doc: 'Files',
     format: String,
     default: `${process.env.PWD}/data`,
